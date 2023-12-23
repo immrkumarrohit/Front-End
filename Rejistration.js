0f7c0366ea6a58c1a287af1e36c9fe4f512a608d
Rohit Kumar <142918146+immrkumarrohit@users.noreply.github.com>
2023-12-24 02:25:55 +0530

    Add files via upload

---
+++ Rejistration.js
@@ -0,0 +1,21 @@
+
+function valueFunction(){
+var name = document.getElementById("name").value;
+
+var number=document.getElementById("numberr").value;
+
+var email =document.getElementById("emails").value;
+
+
+// var final_Name = document.getElementById("name","numberr","emails").value;
+// document.getElementsById("details").innerHTML=final_Name;
+
+
+document.write("Your Name Is =" +name +"<br>");
+document.write("your number is =" +number +"<br>");
+document.write("your email is =" +email);
+alert("Your Name Is = " +name+"   Nd number is = " + numberr+" Nd yr email is= " + emails );
+
+
+}
+
